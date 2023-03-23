748454228a6c11e05cc25578862a9f5e447a846f
Siwa <siwarjsa@gmail.com>
2023-03-23 11:09:42 +0800

    Initial Commit

---
+++ src/main/java/com/mow/MealsOnWheelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.mow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealsOnWheelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealsOnWheelsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mow/MealsOnWheelsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealsOnWheelsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
