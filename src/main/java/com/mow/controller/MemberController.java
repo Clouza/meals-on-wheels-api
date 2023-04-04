789f2f73f90588f167d886643b0e6dce051d2019
Adi Astawa <adiast457@gmail.com>
2023-04-04 18:41:35 +0800

    added method to add member evidence to the database and project directory

--- src/main/java/com/mow/controller/MemberController.java
+++ src/main/java/com/mow/controller/MemberController.java
@@ -3,13 +3,19 @@ package com.mow.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mow.entity.Members;
 import com.mow.entity.Users;
@@ -18,6 +24,15 @@ import com.mow.response.JSONResponse;
 import com.mow.service.MembersService;
 import com.mow.service.UsersService;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
@@ -41,16 +56,77 @@ public class MemberController {
 		return membersService.getRecords();
 	}
 	
-	@PostMapping("/request")
-	public ResponseEntity<?> postRequest(@RequestBody MembersRequest membersRequest) {
-		Users user = usersService.findByUsername(membersRequest.getUsername());
+//	@PostMapping("/request")
+//	public ResponseEntity<?> postRequest(@RequestBody MembersRequest membersRequest) throws IOException {
+//		Users user = usersService.findByUsername(membersRequest.getUsername());
+//		
+//		Members member = new Members();
+//		member.setEvidence(membersRequest.getEvidence());
+//		member.setUser(user);
+//		
+//		membersService.save(member);
+//		
+//		String filename = membersRequest.getImg().getOriginalFilename();
+//	    String path = "target/classes/static/images/member";
+//	    Path dir = Paths.get(path);
+//        if(!Files.exists(dir)) {
+//            Files.createDirectories(dir);
+//        }
+//        
+//        try {
+//            InputStream inputStream = membersRequest.getImg().getInputStream();
+//            Path filePath = dir.resolve(filename);
+//            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException ix) {
+//            System.out.println("Image err " + ix);
+//        }
+//        System.out.println(membersRequest.getImg());
+//        
+//		return new ResponseEntity<>(JSON.stringify("Request has been saved"), HttpStatus.CREATED);
+//	}
+	
+	
+	@PostMapping("/upload-evidence")
+	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file,@RequestParam("username") String username) throws IOException {
+//	    Uploading image to project directory
+		String filename = file.getOriginalFilename();
+	    String path = "target/classes/static/images/member";
+	    Path dir = Paths.get(path);
+        if(!Files.exists(dir)) {
+            Files.createDirectories(dir);
+        }
+        
+        try {
+            InputStream inputStream = file.getInputStream();
+            Path filePath = dir.resolve(filename);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ix) {
+            System.out.println("Image err " + ix);
+        }
+        
+//      Save User Evidence to the database
+        Users user = usersService.findByUsername(username);
 		
 		Members member = new Members();
-		member.setEvidence(membersRequest.getEvidence());
+		member.setEvidence(filename);
 		member.setUser(user);
 		
 		membersService.save(member);
-		return new ResponseEntity<>(JSON.stringify("Request has been saved"), HttpStatus.CREATED);
+        return ResponseEntity.ok().body("File uploaded successfully!");
 	}
 	
+	@GetMapping("/get-image")
+	public ResponseEntity<Resource> getImage(@RequestParam String imageName) throws IOException {
+	    String path = "target/classes/static/images/member";
+	    Path imageFilePath = Paths.get(path).resolve(imageName);
+	    Resource imageResource = new FileSystemResource(imageFilePath.toFile());
+
+	    if (imageResource.exists() && imageResource.isReadable()) {
+	        return ResponseEntity.ok()
+	                .contentType(MediaType.IMAGE_JPEG)
+	                .body(imageResource);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
 }
--- src/main/java/com/mow/request/MembersRequest.java
+++ src/main/java/com/mow/request/MembersRequest.java
@@ -1,5 +1,7 @@
 package com.mow.request;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +13,5 @@ public class MembersRequest {
 
 	String username;
 	String evidence;
-	
+	MultipartFile img;
 }
