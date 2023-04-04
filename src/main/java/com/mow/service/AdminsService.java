8967372eeb7024e2db61fe40aff9c57eaa974101
Siwa <siwarjsa@gmail.com>
2023-04-04 14:03:10 +0800

    add enhanced search for meals

--- src/main/java/com/mow/service/AdminsService.java
+++ src/main/java/com/mow/service/AdminsService.java
@@ -1,13 +1,17 @@
 package com.mow.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mow.entity.Admins;
+import com.mow.entity.Donators;
 import com.mow.entity.Members;
 import com.mow.entity.Partners;
 import com.mow.entity.Riders;
 import com.mow.repository.AdminsRepository;
+import com.mow.repository.DonatorsRepository;
 import com.mow.repository.MembersRepository;
 import com.mow.repository.PartnersRepository;
 import com.mow.repository.RidersRepository;
@@ -25,10 +29,13 @@ public class AdminsService {
 	MembersRepository membersRepository;
 	
 	@Autowired
+	PartnersRepository partnerRepository;
+	
+	@Autowired
 	RidersRepository ridersRepository;
 	
 	@Autowired
-	PartnersRepository partnerRepository;
+	DonatorsRepository donatorsRepository;
 	
 	public void save(Admins admin) {
 		adminsRepository.save(admin);
@@ -52,5 +59,45 @@ public class AdminsService {
 			partners.setApproved(true);
 			partnerRepository.save(partners);
 		}
+	}
+	
+	public List<Admins> getAdmins() {
+		return adminsRepository.findAll();
+	}
+	
+	public Admins getAdminById(Long id) {
+		return adminsRepository.findById(id).get();
+	}
+	
+	public List<Members> getMembers() {
+		return membersRepository.findAll();
+	}
+	
+	public Members getMemberById(Long id) {
+		return membersRepository.findById(id).get();
+	}
+	
+	public List<Partners> getPartners() {
+		return partnerRepository.findAll();
+	}
+	
+	public Partners getPartnerById(Long id) {
+		return partnerRepository.findById(id).get();
+	}
+	
+	public List<Riders> getRiders() {
+		return ridersRepository.findAll();
+	}
+	
+	public Riders getRiderById(Long id) {
+		return ridersRepository.findById(id).get();
+	}
+	
+	public List<Donators> getDonators() {
+		return donatorsRepository.findAll();
+	}
+	
+	public Donators getDonatorById(Long id) {
+		return donatorsRepository.findById(id).get();
 	}
 }
---
+++ src/test/java/com/mow/SearchTests.java
@@ -0,0 +1,66 @@
+package com.mow;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mow.entity.Meals;
+import com.mow.repository.MealsRepository;
+import com.mow.search.MealsSpecification;
+import com.mow.search.SearchCriteria;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class SearchTests {
+
+	@Autowired
+	private MealsRepository mealsRepository;
+	
+	@Test
+	void searchByFieldNotNull() {
+		MealsSpecification mealsSpecification = new MealsSpecification(new SearchCriteria("name", "=", "fc"));
+		List<Meals> meals = mealsRepository.findAll(mealsSpecification);
+		
+		assertNotNull(meals);
+	}
+	
+	@Test
+	void searchByFieldNull() {
+		MealsSpecification mealsSpecification = new MealsSpecification(new SearchCriteria("name", "=", "dummy"));
+		List<Meals> meals = mealsRepository.findAll(mealsSpecification);
+		
+		assertTrue(meals.isEmpty());
+	}
+	
+	@Test
+	void searchWithLike() {
+		MealsSpecification mealsSpecification = new MealsSpecification(new SearchCriteria("name", "%", "fc"));
+		List<Meals> meals = mealsRepository.findAll(mealsSpecification);
+		
+		assertFalse(meals.isEmpty());
+	}
+	
+	@Test
+	void searchByEqualStock() {
+		MealsSpecification mealsSpecification = new MealsSpecification(new SearchCriteria("stock", ">=", "777"));
+		List<Meals> meals = mealsRepository.findAll(mealsSpecification);
+		
+		assertFalse(meals.isEmpty());
+	}
+	
+	@Test
+	void searchByStock() {
+		MealsSpecification mealsSpecification = new MealsSpecification(new SearchCriteria("stock", ">", "999"));
+		List<Meals> meals = mealsRepository.findAll(mealsSpecification);
+		
+		assertTrue(meals.isEmpty());
+	}
+	
+}
